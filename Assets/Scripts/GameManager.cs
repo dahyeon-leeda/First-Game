207f8bdf214dca45574bd69c4dc4347050f081c4
dahyeon-da <lsjin82615160@gmail.com>
2026-02-27 15:36:29 +0900

    ✨feat: System 플레이어 텔레포트 이동
    플레이어가 텔레포트에 닿았을때 다음 레벨 중 한가지 맵으로 이동되면서 스폰포인트로 이동

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject level0Map;      // map1
+    public GameObject[] level1Maps;   // map2, map3
+
+    public PlayerMove player;
+
+    GameObject currentMap;
+
+    void Start()
+    {
+        currentMap = level0Map;
+        level0Map.SetActive(true);
+
+        foreach (GameObject map in level1Maps)
+            map.SetActive(false);
+    }
+
+    public void MoveToLevel1Random()
+    {
+        // 현재 맵 끄기
+        currentMap.SetActive(false);
+
+        // 레벨1 중 랜덤 선택
+        int randomIndex = Random.Range(0, level1Maps.Length);
+
+        currentMap = level1Maps[randomIndex];
+        currentMap.SetActive(true);
+
+        PlayerReposition();
+    }
+
+    void PlayerReposition()
+    {
+        GameObject spawnPoint = GameObject.FindWithTag("SpawnPoint");
+
+        if (spawnPoint == null)
+        {
+            return;
+        }
+
+        player.transform.position = spawnPoint.transform.position;
+        player.VelocityZero();
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    GameManager gameManager;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Finish"))
+        {
+            gameManager.MoveToLevel1Random();
+        }
+    }
+
+    public void VelocityZero()
+    {
+        rigid.linearVelocity = Vector2.zero;
+    }
+}
